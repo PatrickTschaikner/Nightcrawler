78aabc8e6341ec99a02a5e52a1c7a35a74c08b09
PatrickTschaikner <patrick.tschaikner@student.htldornbirn.at>
2024-03-06 09:33:36 +0100

    e

---
+++ assets/Tilemap/industrie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="industrie" tilewidth="32" tileheight="32" tilecount="128" columns="64">
+ <image source="../../../../Downloads/craftpix-net-965049-free-industrial-zone-tileset-pixel-art/map/unnamed.png" width="2048" height="64"/>
+</tileset>
