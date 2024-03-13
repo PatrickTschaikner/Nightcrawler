10a7ce92c02699ccbedc1a0fe9fa5c04ec1727d2
Meehmeet <147132541+Meehmeet@users.noreply.github.com>
2024-03-13 09:15:33 +0100

    asd

--- assets/Tilemap/snow_background.tsx
+++ assets/Tilemap/snow_background.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="snow_background" tilewidth="16" tileheight="16" tilecount="154" columns="22">
- <image source="../../../../OneDrive/Bilder/Aseprite/Patrick/16x16/mountains_landscape.png" width="360" height="120"/>
+ <image source="mountains_landscape.png" width="360" height="120"/>
 </tileset>
