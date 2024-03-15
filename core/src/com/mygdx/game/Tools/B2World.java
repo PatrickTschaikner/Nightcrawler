3e8079b98ad4c44a427c18d094b64e3261da49ef
Meehmeet <147132541+Meehmeet@users.noreply.github.com>
2024-03-15 09:06:45 +0100

    ok

--- core/src/com/mygdx/game/Tools/B2World.java
+++ core/src/com/mygdx/game/Tools/B2World.java
@@ -14,6 +14,18 @@ public class B2World {
         FixtureDef fdef = new FixtureDef();
         Body body;
 
+        Body groundBody = world.createBody(bdef);
+        PolygonShape groundshape = new PolygonShape();
+        groundshape.setAsBox(30, 1.0f);
+        FixtureDef groundFixture = new FixtureDef();
+        groundFixture.density=0.0f;
+        groundFixture.shape = groundshape;
+        groundFixture.restitution = .5f;
+        groundFixture.friction=0f;
+        groundBody.createFixture(groundFixture);
+        groundshape.dispose();
+
+
         for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
             bdef.type = BodyDef.BodyType.StaticBody;
